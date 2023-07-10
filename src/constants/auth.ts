ec58f5dcea4fed32cd8a70e784998626c03df0b3
mxrk314 <136643669+mxrk314@users.noreply.github.com>
2023-07-10 20:58:07 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,4 @@
-export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+export const officialAPIEndpoint = 'https://chimeragpt.adventblocks.cc/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 export const defaultAPIEndpoint =
