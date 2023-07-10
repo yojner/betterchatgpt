21944b6cd3a55a768b24f026b228ae4ce26a9ed0
mxrk314 <136643669+mxrk314@users.noreply.github.com>
2023-07-10 20:44:40 +0800

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -36,7 +36,7 @@ export const modelMaxToken = {
   'gpt-3.5-turbo-0301': 4096,
   'gpt-3.5-turbo-0613': 4096,
   'gpt-3.5-turbo-16k': 16384,
-  'gpr-3.5-turbo-16k-openai': 16384,
+  'gpt-3.5-turbo-16k-openai': 16384,
   'gpt-3.5-turbo-16k-0613': 16384,
   'gpt-4': 16384,
   'gpt-4-0314': 8192,
